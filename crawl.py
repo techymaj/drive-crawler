c87c2f21cdfc08df9d365dab34626d17f22d4b19
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-22 17:33:23 +0300

    walk()

--- crawl.py
+++ crawl.py
@@ -4,6 +4,8 @@ from color_codes import *
 directory = "/media/kali/256"
 for dir_list in os.listdir(directory):
     if os.path.isdir(f"{directory}/{dir_list}"):
-        print_ic(f"Folders in drive: {directory}/ {dir_list}", BLUE)
+        for item in os.walk(f"{directory}/{dir_list}"):
+            print(item)
+        # print_ic(f"Folders in drive: {directory}/ {dir_list}", BLUE)
     else:
         print_ic(f"Files in drive: {directory}/ {dir_list}")
