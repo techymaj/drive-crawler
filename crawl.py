38027be0dc78748047f2169bf5d3c0eb1b39f4ff
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-22 17:50:20 +0300

    walk more

--- crawl.py
+++ crawl.py
@@ -4,8 +4,7 @@ from color_codes import *
 directory = "/media/kali/256"
 for dir_list in os.listdir(directory):
     if os.path.isdir(f"{directory}/{dir_list}"):
-        for item in os.walk(f"{directory}/{dir_list}"):
-            print(item)
-        # print_ic(f"Folders in drive: {directory}/ {dir_list}", BLUE)
-    else:
-        print_ic(f"Files in drive: {directory}/ {dir_list}")
+        for dirpath, dirnames, filenames in os.walk(f"{directory}/{dir_list}"):
+            print(f"Current Directory: {dirpath}")
+            print(f"Subdirectories: {dirnames}")
+            print(f"Files: {filenames}")
