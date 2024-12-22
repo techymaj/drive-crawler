fa32eb85b70bde0833daa85e00bd3c521a4e66aa
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-22 17:24:46 +0300

    more verbose

--- crawl.py
+++ crawl.py
@@ -4,4 +4,6 @@ from color_codes import *
 directory = "/media/kali/256"
 for dir_list in os.listdir(directory):
     if os.path.isdir(f"{directory}/{dir_list}"):
-        print_ic(f"In drive: {directory}/ {dir_list}")
+        print_ic(f"Folders in drive: {directory}/ {dir_list}")
+    else:
+        print_ic(f"Files in drive: {directory}/ {dir_list}")
