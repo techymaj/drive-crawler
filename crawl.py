5d10126edc3fb7cfab906bd73fc14e2b59c66aaa
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-22 16:39:44 +0300

    Listing

--- crawl.py
+++ crawl.py
@@ -2,8 +2,7 @@ import os
 
 directory = "/media/kali/256"
 for file in os.listdir(directory):
-    file_path = os.path.join(directory, file)
-    if os.path.isfile(file_path):  # Only process files
-        with open(file_path, "r") as f:
-            content = f.read()
-            print(f"Contents of {file_path}:\n{content}")
+    if os.path.isfile(file):
+        print(f"File: {file}")
+    else:
+        print(f"Directory: {file}")
