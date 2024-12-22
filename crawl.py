3f19da598d3c2c5cc596eb3ef4b5285139889315
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-22 17:54:05 +0300

    walk more

--- crawl.py
+++ crawl.py
@@ -5,6 +5,6 @@ directory = "/media/kali/256"
 for dir_list in os.listdir(directory):
     if os.path.isdir(f"{directory}/{dir_list}"):
         for dirpath, dirnames, filenames in os.walk(f"{directory}/{dir_list}"):
-            print(f"Current Directory: {dirpath}")
+            print(f"\nCurrent Directory: {dirpath}")
             print(f"Subdirectories: {dirnames}")
             print(f"Files: {filenames}")
