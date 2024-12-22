743835ee2392053b40c853af86d01d813e90782e
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-22 16:42:40 +0300

    Listing

--- crawl.py
+++ crawl.py
@@ -2,7 +2,4 @@ import os
 
 directory = "/media/kali/256"
 for file in os.listdir(directory):
-    if os.path.isfile(file):
-        print(f"File: {file}")
-    else:
-        print(f"Directory: {file}")
+    print(f"In drive: {directory}/ {file}")
