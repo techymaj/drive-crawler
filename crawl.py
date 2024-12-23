3ae4326fe2ff50f50d17a5ba3acdc0ed3b460597
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-23 17:29:33 +0300

    Added logger

--- crawl.py
+++ crawl.py
@@ -2,8 +2,13 @@ import os
 from color_codes import *
 
 directory = "/media/kali"
+log_crawl = "drive_crawler_log.txt"
 
 for dirpath, dirnames, filenames in os.walk(f"{directory}"):
     print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
-    print_ic(f"Subdirectories: {dirnames}", CYAN)
-    print(f"Files: {filenames}")
+    print_ic(f"\tSubdirectories: {dirnames}", CYAN)
+    print(f"\t\tFiles: {filenames}")
+    with open(log_crawl, "a") as drive:
+        drive.write(f"\nCurrent Directory: {dirpath}")
+        drive.write(f"\tSubdirectories: {dirnames}")
+        drive.write(f"\t\tFiles: {filenames}")
