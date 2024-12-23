f308decc654513996c740dbfe7c8fcb4291fa827
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-23 15:42:32 +0300

    tweaks

--- crawl.py
+++ crawl.py
@@ -6,12 +6,12 @@ path = "/media/kali/256"
 with os.scandir(path) as entries:
     for entry in entries:
         name = entry.name
-        path = entry.path
+        dir_path = entry.path
         print(f"Name: {name}")
         print(f"Path: {path}")
         print("-" * 80)
 
-    for dirpath, dirnames, filenames in os.walk(f"{path}"):
-        print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
-        print_ic(f"Subdirectories: {dirnames}", CYAN)
-        print(f"Files: {filenames}")
+        for dirpath, dirnames, filenames in os.walk(f"{dir_path}"):
+            print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
+            print_ic(f"Subdirectories: {dirnames}", CYAN)
+            print(f"Files: {filenames}")
