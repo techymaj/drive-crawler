e42f307b2e58853d2245f3897f445325127e2ee1
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-23 15:22:58 +0300

    Switched to os.scandir

--- crawl.py
+++ crawl.py
@@ -1,10 +1,14 @@
 import os
 from color_codes import *
 
-directory = "/media/kali/256"
-for dir_list in os.listdir(directory):
-    if os.path.isdir(f"{directory}/{dir_list}"):
-        for dirpath, dirnames, filenames in os.walk(f"{directory}/{dir_list}"):
-            print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
-            print_ic(f"Subdirectories: {dirnames}", CYAN)
-            print(f"Files: {filenames}")
+path = "/media/kali/256"
+# for dir_list in os.listdir(directory):
+#     if os.path.isdir(f"{directory}/{dir_list}"):
+#         for dirpath, dirnames, filenames in os.walk(f"{directory}/{dir_list}"):
+#             print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
+#             print_ic(f"Subdirectories: {dirnames}", CYAN)
+#             print(f"Files: {filenames}")
+
+with os.scandir(path) as entries:
+    for entry in entries:
+        print(entry)
