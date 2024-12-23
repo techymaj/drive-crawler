1e159b8dd2c9d82daafea4bcf41b9dfb4ed19e67
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-23 19:08:25 +0300

    refactor

--- crawl.py
+++ crawl.py
@@ -1,3 +1,4 @@
+import json
 import os
 from color_codes import *
 
@@ -7,22 +8,28 @@ log_crawl = "drive_crawler_log.txt"
 tree = {}
 
 for dirpath, dirnames, filenames in os.walk(f"{directory}"):
-    print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
-    print_ic(f"\tSubdirectories: {dirnames}", CYAN)
-    print(f"\t\tFiles: {filenames}")
+    # print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
+    # print_ic(f"\tSubdirectories: {dirnames}", CYAN)
+    # print(f"\t\tFiles: {filenames}")
+    #
+    # if len(filenames) == 0:
+    #     tree.update(
+    #         {
+    #             dirpath : {
+    #                 directory: filenames for directory in dirnames
+    #             }
+    #         }
+    #     )
+    parts = dirpath.split(os.sep)
+    subtree = tree
+    for part in parts:
+        subtree = subtree.setdefault(part, {})
+    subtree.update({"_files": filenames})
 
-    tree.update(
-        {
-            dirpath : {
-                directory: filenames for directory in dirnames
-            }
-        }
-    )
+    # with open(log_crawl, "a") as drive:
+    #     drive.write(f"\nCurrent Directory: {dirpath}")
+    #     drive.write(f"\n\tSubdirectories: {dirnames}")
+    #     drive.write(f"\n\t\tFiles: {filenames}")
 
-    with open(log_crawl, "a") as drive:
-        drive.write(f"\nCurrent Directory: {dirpath}")
-        drive.write(f"\n\tSubdirectories: {dirnames}")
-        drive.write(f"\n\t\tFiles: {filenames}")
-
-with open("tree.json", "w") as json:
-    print(tree, file=json)
+with open("tree.json", "w") as json_file:
+    json.dump(tree, json_file, indent=4)
