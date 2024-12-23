94f508b069a4f66a7016101d26d253ac9888f190
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-23 15:48:25 +0300

    Cleanup

--- crawl.py
+++ crawl.py
@@ -1,17 +1,9 @@
 import os
 from color_codes import *
 
-path = "/media/kali/256"
+directory = "/media/kali/256"
 
-with os.scandir(path) as entries:
-    for entry in entries:
-        name = entry.name
-        dir_path = entry.path
-        print(f"Name: {name}")
-        print(f"Path: {path}")
-        print("-" * 80)
-
-        for dirpath, dirnames, filenames in os.walk(f"{dir_path}"):
-            print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
-            print_ic(f"Subdirectories: {dirnames}", CYAN)
-            print(f"Files: {filenames}")
+for dirpath, dirnames, filenames in os.walk(f"{directory}"):
+    print_ic(f"\nCurrent Directory: {dirpath}", RED, BOLD)
+    print_ic(f"Subdirectories: {dirnames}", CYAN)
+    print(f"Files: {filenames}")
